d319828126e06eba0b2884ea624d2e316bc0a1b8
heshashawky <heshamwebdeve@gmail.com>
2016-04-06 18:16:29 +0200

    vestudio

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var $;
+$(function () {
+    "use strict";
+
+    $('.carousel').carousel({
+        interval: false
+    });
+
+    $('.Collage li').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        // other options
+
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function (item) {
+                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+            }
+        },
+
+        zoom: {
+            enabled: true,
+            duration: 300, // don't foget to change the duration also in CSS
+            opener: function (element) {
+                return element.find('img');
+            }
+        }
+    });
+
+});
